e53c54a6c60ad068e036de0f303747f75f106ac3
Steven Marshall <SMarshall@chef.io>
2020-06-23 13:49:35 +0100

    [rq] Various refactors of existing code
    Added inspec.lock to .gitignore
    Updated README.md to latest binary format
    Added plan_name to attributes.yml
    Updates controls to add more prescriptive version checking.
    Updated inspec.yml to latest version
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/rq_works.rb
+++ controls/rq_works.rb
@@ -9,28 +9,34 @@ control 'core-plans-rq' do
   impact 1.0
   title 'Ensure rq binary is working as expected'
   desc '
-We first check that the rq binary we expect is present and then run a version check to verify that it is excecutable.
+  To test the rq binary that core/rq provides we first check for the installation directory.
+  Using this directory we then run checks to ensure the binary exists.
+  Then we test that the version of the binary we expect to be installed exists.
+    $ $PKG_PATH/bin/rq --version
+      v0.10.4
   '
 
   hab_pkg_path = command("hab pkg path #{plan_ident}")
   describe hab_pkg_path do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
   end
 
   target_dir = File.join(hab_pkg_path.stdout.strip, base_dir)
 
-  rq_exists = command("ls #{File.join(target_dir, "rq")}")
+  rq_exists = command("ls -al #{File.join(target_dir, "rq")}")
   describe rq_exists do
     its('stdout') { should match /rq/ }
     its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 
-  rq_works = command("/bin/rq --version")
+  rq_works = command("#{File.join(target_dir, "rq")} --version")
   describe rq_works do
-    its('stdout') { should match /v[0-9]+.[0-9]+.[0-9]+/ }
+    its('stdout') { should match /v#{hab_pkg_path.stdout.strip.split('/')[5]}/ }
     its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
+
 end

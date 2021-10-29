d4e1b5531987d1ad80e38b415e27683103f409f4
Nimit <nimit.jyotiana@hotmail.com>
2021-10-29 21:55:17 +0530

    upgraded version to v1.0.2
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- plan.sh
+++ plan.sh
@@ -1,6 +1,6 @@
 pkg_name=rq
 pkg_origin=core
-pkg_version=0.10.4
+pkg_version=1.0.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 Record Query is a tool for doing record analysis and transformation. It's \
@@ -8,8 +8,8 @@ used for performing queries on streams of records in various formats.\
 "
 pkg_upstream_url="https://github.com/dflemstr/rq"
 pkg_license=('Apache-2.0')
-pkg_source="https://github.com/dflemstr/rq/releases/download/v${pkg_version}/record-query-v${pkg_version}-x86_64-unknown-linux-gnu.tar.gz"
-pkg_shasum="1b9abedd7acca1269ac1c4765d93bab73777feca9a04358ea4f9c9659bf51e62"
+pkg_source=https://github.com/dflemstr/rq/releases/download/v${pkg_version}/rq-v${pkg_version}-x86_64-unknown-linux-gnu.tar.gz
+pkg_shasum=b870456796c27f358090a5e68944d912beb4280bc7010912cb3d6753f2da936c
 pkg_deps=(
   core/glibc
   core/gcc-libs
@@ -19,22 +19,13 @@ pkg_build_deps=(
 )
 pkg_bin_dirs=(bin)
 
-do_unpack() {
-  do_default_unpack
-
-  # The source tarball contains a cross-compiled binary in a parent directory
-  # with a vague name. here we'll rename it to the hab standard pkg_dirname
-  # instead of setting pkg_dirname to this vague name.
-  mv -v "${HAB_CACHE_SRC_PATH}/x86_64-unknown-linux-gnu" \
-    "${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
-}
-
 do_build() {
-  return 0
+	return 0
 }
 
 do_install() {
-  install -v "rq" "$pkg_prefix/bin/$pkg_name"
+	mv "${HAB_CACHE_SRC_PATH}/rq" "${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
+	cp "${HAB_CACHE_SRC_PATH}/${pkg_dirname}/rq" "${pkg_prefix}/bin"
 }
 
 do_strip() {
